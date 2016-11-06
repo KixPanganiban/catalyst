02b17892ba8a401d54b0f2287e71e98b5b6cf8f0
Kix Panganiban <kixpanganiban@protonmail.com>
2016-11-06 10:37:39 +0800

    Initial commit.

---
+++ web/router.ex
@@ -0,0 +1,26 @@
+defmodule Catalyst.Router do
+  use Catalyst.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Catalyst do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Catalyst do
+  #   pipe_through :api
+  # end
+end
